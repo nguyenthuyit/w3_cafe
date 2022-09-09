66d8bf2c2f12e2b9c5033dc83784f05572a25c76
nguyenthuyit <nguyenthithuy.int@gmail.com>
2022-09-09 22:06:21 +0700

    upload

--- menuFood.js
+++ menuFood.js
@@ -3,3 +3,19 @@
 const now = new Date();
 now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
 document.getElementById('dt').value = now.toISOString().slice(0, 16);
+
+// food-menu
+
+function openMenu(evt, namefood){
+    var i, tabfoods, foods
+    foods = document.getElementsByClassName('food')
+    for(i = 0; i < foods.length; i++){
+        foods[i].style.display = 'none'  
+    }
+    tabfoods =document.getElementsByClassName('tabfood')
+    for(i = 0; i < foods.length; i++){
+        tabfoods[i].className = tabfoods[i].className.replace(' back-color','')
+    }
+    document.getElementById(namefood).style.display = 'block'
+    evt.currentTarget.className += ' back-color'
+}
